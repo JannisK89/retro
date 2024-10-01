9f27e329c929e30b01cf69ebb058086a6a36f7c8
JannisK89 <jannis.karanikis@gmail.com>
2024-10-01 19:27:56 +0200

    project init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let boardName = '';
+</script>
+
+<div class="flex items-center justify-center h-screen">
+	<div class="space-y-5">
+		<h1 class="h1">Welcome to Retro!</h1>
+		<p>Create a retro board to get started!</p>
+
+<form   
+		<label for="board-name" class="sr-only">Board Name</label>
+		<input
+			type="text"
+			id="board-name"
+			name="board-name"
+			class="input"
+			placeholder="Board Name"
+			bind:value={boardName}
+		/>
+	</div>
+</div>
