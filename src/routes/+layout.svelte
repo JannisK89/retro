68329bbb1c3e2967a7abcfb817650ee54872700d
JannisK89 <jannis.karanikis@gmail.com>
2024-10-07 19:28:21 +0200

    supabase auth

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
 	import '../app.postcss';
+
+	import { invalidate } from '$app/navigation';
+	import { onMount, type Snippet } from 'svelte';
+	import type { Session, SupabaseClient } from '@supabase/supabase-js';
 	interface Props {
-		children?: import('svelte').Snippet;
+		children?: Snippet;
+		session: Session | null;
+		supabase: SupabaseClient;
 	}
 
-	let { children }: Props = $props();
+	let data = $props();
+	let { session, supabase }: Props = $derived(data);
+	let { children } = data;
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
 <div class="flex items-center justify-center h-screen">
