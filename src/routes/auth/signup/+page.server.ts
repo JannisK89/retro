4a274016a2b8f7a0590062e826695a47af55d08d
JannisK89 <jannis.karanikis@gmail.com>
2024-11-09 13:04:32 +0100

    sign up page and validation

---
+++ src/routes/auth/signup/+page.server.ts
@@ -0,0 +1,42 @@
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
+import { z } from 'zod';
+import { fromError } from 'zod-validation-error';
+
+const signUpSchema = z.object({
+	email: z.string().email().max(255),
+	username: z.string().min(1).max(255),
+	password: z.string().min(8).max(255)
+});
+
+export const actions: Actions = {
+	signup: async ({ request, locals: { supabase } }) => {
+		const formData = await request.formData();
+		const email = formData.get('email') as string;
+		const password = formData.get('password') as string;
+		const passwordConfirm = formData.get('passwordConfirm') as string;
+		const username = formData.get('username') as string;
+
+		if (password !== passwordConfirm) {
+			return fail(400, { message: 'Passwords do not match' });
+		}
+
+		const result = signUpSchema.safeParse({ email, username, password });
+		if (!result.success) {
+			const validationError = fromError(result.error).toString();
+			return fail(400, { message: validationError });
+		}
+
+		const { error } = await supabase.auth.signUp({
+			email,
+			password,
+			options: { data: { username } }
+		});
+		if (error) {
+			console.error(error);
+			return fail(500, { message: 'Failed to sign up' });
+		} else {
+			redirect(303, '/board');
+		}
+	}
+};
---
+++ src/routes/auth/signup/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+
+	let { form }: { form: { message: string } } = $props();
+</script>
+
+<h2 class="h2">Sign Up</h2>
+<form method="POST" class="flex flex-col gap-2 w-80 lg:w-96" use:enhance>
+	<label class="label" for="email"> Email </label>
+	<input class="input p-2" name="email" type="email" id="email" placeholder="Email" required />
+	<label class="label" for="username"> Username </label>
+	<input
+		class="input p-2"
+		name="username"
+		type="text"
+		id="username"
+		placeholder="Username"
+		required
+		min="1"
+	/>
+	<label class="label" for="password"> Password (Minimum 8 Characters)</label>
+	<input
+		class="input p-2"
+		name="password"
+		type="password"
+		id="password"
+		placeholder="Password"
+		required
+		min="8"
+	/>
+	<label class="label" for="passwordConfirm"> Confirm Password </label>
+	<input
+		class="input p-2"
+		name="passwordConfirm"
+		type="password"
+		id="passwordConfirm"
+		placeholder="Confirm Password"
+		required
+		min="8"
+	/>
+	<div class="flex justify-around gap-4 mt-4">
+		<button class="btn btn-lg variant-filled w-full" formaction="?/signup">Sign up</button>
+	</div>
+	{#if form?.message}
+		{#each form.message.split(';') as message}
+			<p class="max-w-max text-warning-600 text-wrap">{message}</p>
+		{/each}
+	{/if}
+</form>
